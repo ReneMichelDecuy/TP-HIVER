63bf39700601a8c1a830b909a9d76a3fe0462cec
Manuuzz <manu60120@gmail.com>
2022-05-03 17:18:27 +0200

    ailier change

--- Classes/Ailier.php
+++ Classes/Ailier.php
@@ -1,4 +1,12 @@
 <?php class Ailier{
+    private $id_;
+    private $prenom_;
+    private $nom_;
 
+    public function __construct($id, $prenom, $nom){
+        $this->id_ =$id;
+        $this->prenom_ =$prenom;
+        $this->nom_ =$nom;
+    }
 }
 ?>
